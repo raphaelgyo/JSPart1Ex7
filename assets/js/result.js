6ef39a5843b28217a04b2162b4453a6638997ee0
raphaelgyo <raphael.granizo@gmail.com>
2019-11-27 13:04:04 +0100

    2nd

--- assets/js/result.js
+++ assets/js/result.js
@@ -1,1 +1,11 @@
-var result = firstNumber / secondNumber
+var validation = document.getElementById('submit');
+validation.addEventListener('click', f_valid);
+function f_valid() {
+// Déclaration des variables
+var age = document.getElementById('age').value;
+// on compare l'âge : s'il est supérieur ou égal à 18 ans, alors le visiteur est majeur
+if(age >= 18)
+   alert('Vous êtes majeur');
+else
+   alert('T\'es mineur');
+}
