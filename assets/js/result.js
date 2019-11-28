263028694df50484bec3613e30dd116b3b7c7238
raphaelgyo <raphael.granizo@gmail.com>
2019-11-28 09:24:53 +0100

    commentaires

--- assets/js/result.js
+++ assets/js/result.js
@@ -1,11 +1,14 @@
 var validation = document.getElementById('submit');
 validation.addEventListener('click', f_valid);
-function f_valid() {
-// Déclaration des variables
-var age = document.getElementById('age').value;
-// on compare l'âge : s'il est supérieur ou égal à 18 ans, alors le visiteur est majeur
-if(age >= 18)
-   alert('Vous êtes majeur');
-else
-   alert('T\'es mineur');
+function f_valid(){
+  //Déclaration de la variable age
+  var age = document.getElementById('age').value;
+  // on compare la valeur de la variable age : si elle est supérieure ou égale à 18, alors le visiteur est majeur
+  if(age >= 18){
+    alert("Vous êtes majeur, vous pouvez accéder à notre site");
+  }
+  //si la valeur de la variable age est inférieure à 18, alors le visiteur est mineur
+  else{
+    alert("Vous êtes mineur, dégagez !");
+  }
 }
